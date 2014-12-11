99b06c85fe63d38f5937ec791b68cfb94f4eb676
Naruse Motoki <motoki@naru.se>
2014-12-11 12:33:36 +0900

    Optimize Go

--- srv/go/main.go
+++ srv/go/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"encoding/json"
-	"io/ioutil"
 	"strings"
 )
 
@@ -17,11 +15,11 @@ type response_json struct {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	body, _ := ioutil.ReadAll(r.Body)
+	decoder := json.NewDecoder(r.Body)
 	var rj request_json
-	json.Unmarshal(body, &rj)
+	decoder.Decode(&rj)
 	a, _ := json.Marshal(response_json{len(strings.Split(rj.Key, " "))})
-	fmt.Fprintf(w, string(a))
+	w.Write(a)
 }
 
 func main() {
